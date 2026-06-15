9687194e29ecd28babf72dd9a72449ac06545fc1
Santhiya Sri.C.S <jayanthiseenivasan1325_bcs27@mepcoeng.ac.in>
2026-06-15 23:37:45 +0530

    Update Max Sum Submatrix.py

--- Amazon Problems/Max Sum Submatrix.py
+++ Amazon Problems/Max Sum Submatrix.py
@@ -1,34 +1,20 @@
 def kadane(arr):
-
     current = arr[0]
     best = arr[0]
-
     for i in range(1, len(arr)):
         current = max(arr[i], current + arr[i])
         best = max(best, current)
-
     return best
-
-
 r = int(input("Rows: "))
 c = int(input("Columns: "))
-
 mat = []
-
 for i in range(r):
     mat.append(list(map(int, input().split())))
-
 result = float('-inf')
-
 for left in range(c):
-
     temp = [0] * r
-
     for right in range(left, c):
-
         for row in range(r):
             temp[row] += mat[row][right]
-
         result = max(result, kadane(temp))
-
 print(result)
