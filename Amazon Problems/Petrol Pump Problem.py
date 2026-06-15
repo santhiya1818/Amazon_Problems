245385e9ba38e099c28cceef02065252baf4719a
Santhiya Sri.C.S <jayanthiseenivasan1325_bcs27@mepcoeng.ac.in>
2026-06-15 23:38:05 +0530

    Update Petrol Pump Problem.py

--- Amazon Problems/Petrol Pump Problem.py
+++ Amazon Problems/Petrol Pump Problem.py
@@ -1,21 +1,15 @@
 n = int(input("Number of Pumps: "))
-
 petrol = list(map(int, input("Petrol: ").split()))
 distance = list(map(int, input("Distance: ").split()))
-
 start = 0
 balance = 0
 deficit = 0
-
 for i in range(n):
-
     balance += petrol[i] - distance[i]
-
     if balance < 0:
         start = i + 1
         deficit += balance
         balance = 0
-
 if balance + deficit >= 0:
     print(start)
 else:
