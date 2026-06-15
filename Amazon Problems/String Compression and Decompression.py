68e8ea9d1636e44a7b8b966111505008951f75a3
Santhiya Sri.C.S <jayanthiseenivasan1325_bcs27@mepcoeng.ac.in>
2026-06-15 23:39:16 +0530

    Update String Compression and Decompression.py

--- Amazon Problems/String Compression and Decompression.py
+++ Amazon Problems/String Compression and Decompression.py
@@ -1,7 +1,6 @@
 def compress(s):
     result = ""
     count = 1
-
     for i in range(1, len(s)):
         if s[i] == s[i - 1]:
             count += 1
@@ -10,39 +9,26 @@ def compress(s):
             if count > 1:
                 result += str(count)
             count = 1
-
     result += s[-1]
     if count > 1:
         result += str(count)
-
     return result
-
-
 def decompress(s):
     result = ""
     i = 0
-
     while i < len(s):
         ch = s[i]
         i += 1
-
         num = ""
-
         while i < len(s) and s[i].isdigit():
             num += s[i]
             i += 1
-
         if num:
             result += ch * int(num)
         else:
             result += ch
-
     return result
-
-
 s = input("Enter String: ")
-
 compressed = compress(s)
-
 print("Compressed:", compressed)
 print("Decompressed:", decompress(compressed))
