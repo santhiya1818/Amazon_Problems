ced64d904df55ea7272485b887ebf5064a6698e2
Santhiya Sri.C.S <jayanthiseenivasan1325_bcs27@mepcoeng.ac.in>
2026-06-15 23:35:58 +0530

    Update Element search in Sorted Matrix.py

--- Amazon Problems/Element search in Sorted Matrix.py
+++ Amazon Problems/Element search in Sorted Matrix.py
@@ -1,28 +1,18 @@
 n = int(input("Rows: "))
 m = int(input("Columns: "))
-
 mat = []
-
 for i in range(n):
     mat.append(list(map(int, input().split())))
-
 x = int(input("Element: "))
-
 row = 0
 col = m - 1
-
 found = 0
-
 while row < n and col >= 0:
-
     if mat[row][col] == x:
         found = 1
         break
-
     elif mat[row][col] > x:
         col -= 1
-
     else:
         row += 1
-
 print(found)
