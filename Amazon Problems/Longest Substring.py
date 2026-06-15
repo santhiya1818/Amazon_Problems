732acc5de3e0138e814d42b7d03d554d3a1bc63d
Santhiya Sri.C.S <jayanthiseenivasan1325_bcs27@mepcoeng.ac.in>
2026-06-15 23:37:18 +0530

    Update Longest Substring.py

--- Amazon Problems/Longest Substring.py
+++ Amazon Problems/Longest Substring.py
@@ -1,16 +1,10 @@
 s = input("Enter String: ")
-
 seen = {}
 start = 0
 ans = 0
-
 for end in range(len(s)):
-
     if s[end] in seen and seen[s[end]] >= start:
         start = seen[s[end]] + 1
-
     seen[s[end]] = end
-
     ans = max(ans, end - start + 1)
-
 print(ans)
